7504b5df9d7dd4e9f4cd7ac6ea7a955e0bb33c9a
Eldos Baktybekov <97338193+usbtypec1@users.noreply.github.com>
2025-11-04 07:25:39 +0600

    Update UniquePaths.java

--- UniquePaths.java
+++ UniquePaths.java
@@ -1,32 +1,32 @@
 import java.util.Scanner;
 
 public class UniquePaths {
-    public static int uniquePaths(int m, int n) {
-        int[][] dp = new int[m][n];
+    public static int uniquePaths(int rows, int columns) {
+        int[][] dp = new int[rows][columns];
 
-        for (int row = 0; row < m; row++) {
+        for (int row = 0; row < rows; row++) {
             dp[row][0] = 1;
         }
-        for (int column = 0; column < n; column++) {
+        for (int column = 0; column < columns; column++) {
             dp[0][column] = 1;
         }
 
-        for (int row = 1; row < m; row++) {
-            for (int column = 1; column < n; column++) {
+        for (int row = 1; row < rows; row++) {
+            for (int column = 1; column < columns; column++) {
                 dp[row][column] = dp[row - 1][column] + dp[row][column - 1];
             }
         }
 
-        return dp[m - 1][n - 1];
+        return dp[rows - 1][columns - 1];
     }
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.err.print("m: ");
-        int m = scanner.nextInt();
-        System.err.print("n: ");
-        int n = scanner.nextInt();
+        System.out.print("rows: ");
+        int rows = scanner.nextInt();
+        System.out.print("columns: ");
+        int columns = scanner.nextInt();
         scanner.close();
-        System.out.println("Unique Paths: " + uniquePaths(m, n));
+        System.out.println("Unique Paths: " + uniquePaths(rows, columns));
     }
 }
